b92a86cbb0c3d90e3c664a57aa3922f23be1c7fd
Pierce Bala <piercebala@gmail.com>
2023-12-26 14:43:23 -0600

    test: implement checksum into Luhn algorithm

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App, { isValidLuhn } from './App';
 
 test('renders app with input and button', () => {
   Object.defineProperty(window, 'matchMedia', {
@@ -21,4 +21,29 @@ test('renders app with input and button', () => {
   const submit = screen.getByTestId('validate-submit');
   expect(input).toBeInTheDocument();
   expect(submit).toBeInTheDocument();
+});
+
+test('computeLuhnAlgorithm validates non-numeric strings', () => {
+  const invalid_string = 'foobar';
+  expect(isValidLuhn(invalid_string)).toEqual(false);
+});
+
+test('computeLuhnAlgorithm validates empty strings', () => {
+  const empty_string = '';
+  expect(isValidLuhn(empty_string)).toEqual(false);
+});
+
+test('isValidLuhn validates American Express cards', () => {
+  const AE_card = '371449635398431';
+  expect(isValidLuhn(AE_card)).toEqual(true);
+});
+
+test('isValidLuhn validates MasterCard cards', () => {
+  const MasterCard_card = '5555555555554444';
+  expect(isValidLuhn(MasterCard_card)).toEqual(true);
+});
+
+test('isValidLuhn validates Visa cards', () => {
+  const Visa_card = '4012888888881881';
+  expect(isValidLuhn(Visa_card)).toEqual(true);
 });
