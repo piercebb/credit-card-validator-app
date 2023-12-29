8da73e856c6411614155009758e7002c72dec030
Pierce Bala <piercebala@gmail.com>
2023-12-29 15:40:49 -0600

    chore: Wrap App in ChakraProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,16 @@
-import { Box, ChakraProvider, Center, Heading } from '@chakra-ui/react';
+import { Box, Center, Heading } from '@chakra-ui/react';
 
 import { CardInput } from './components/CardInput/CardInput';
-import theme from './theme';
 import './App.css';
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
-      <Center className='App' data-testid='app'>
-        <Box p={20}>
-          <Heading p={5}>Validate Credit Card</Heading>
-          <CardInput />
-        </Box>
-      </Center>
-    </ChakraProvider>
+    <Center className='App' data-testid='app'>
+      <Box p={20}>
+        <Heading p={5}>Validate Credit Card</Heading>
+        <CardInput />
+      </Box>
+    </Center>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import theme from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
   </React.StrictMode>
 );
 
